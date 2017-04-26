18782281f34b7dccb248312bf098e0109d8950ed
James <James@James-PC>
2017-04-26 01:47:59 -0400

    xo and mut for submission

--- src/Driver.java
+++ src/Driver.java
@@ -11,13 +11,10 @@ public class Driver {
 		//Reads in from file and inits population
 		//Change Parameters before submitting ---------------
 		City cities[] = Utils.getCities(new File("res/dj38.txt"));
-		int maxGeneration =  100;
-		int populationSize = 100;
-		double rateMut=0.4;
-		double rateXO = 0.7;
-		int XOAdjust[] = new int[2];
-			XOAdjust[0]=maxGeneration-((int) (maxGeneration*rateXO));
-			XOAdjust[1]=maxGeneration-((int) (maxGeneration*rateXO));
+		int maxGeneration =  10000;
+		int populationSize = 1000;
+		double rateMut=0.6;
+		double rateXO = 0.5;
 		int generation=0;
 		Tour solution;
 		int eliteCnt=0;
@@ -28,40 +25,30 @@ public class Driver {
 		//create genetic algorithm object
 		GeneticAlgorithm geneAl = new GeneticAlgorithm(populationSize,rateMut,rateXO, Utils.getCities(new File("res/dj38.txt")));
 		
-		//Initalize Population
+		//Initialize Population
 		int population[][] = geneAl.InitPop(cities.length);
 		double eliteDistance = geneAl.getElite().getTourDistance();
-		System.out.println(eliteDistance);
+		//System.out.println(eliteDistance);
 		
-		//check terminiation conditions
+		//check termination conditions
 		while(generation<maxGeneration){
+			
 			//evaluate population
 			geneAl.selectParents();
+			
 			//perform crosover
 			geneAl.CrossOver(XOSwitch);
-			//geneAl.OX(cities.length);
-			//geneAl.PMX(cities.length);
-			//Preforms Mutation
 			geneAl.mutate();
-			/*if(generation>XOAdjust[0]){
-				if(rateXO>0.2){
-					rateXO-=.1;
-				}
-				geneAl.UpdateXORate(rateXO);
-				XOAdjust[0]+=XOAdjust[1];
-				System.out.println("Generation:"+ generation +" ratexo:"+rateXO);
-				System.out.println(geneAl.getElite() + "\nSwitch Count:" +switchCnt +"\nElite Updates"+updateElite );
-			}*/
 			
 			if (eliteDistance > geneAl.getElite().getTourDistance()){
 				eliteDistance = geneAl.getElite().getTourDistance();
 				eliteCnt=0;
 				updateElite++;
-				System.out.println(geneAl.getElite());
+				//System.out.println(geneAl.getElite());
 			}else{
 				eliteCnt++;
 			}
-			
+			//for changing the crossover algorithms
 			if(eliteCnt>maxGeneration*.01){
 				XOSwitch=!XOSwitch;
 				switchCnt++;
@@ -76,8 +63,8 @@ public class Driver {
 			generation++;
 		}//end generation while
 		
-		System.out.println(geneAl.getElite() + "\nSwitch Count:" +switchCnt +"\nElite Updates"+updateElite );
-		geneAl.showElite();
+		System.out.println(geneAl.getElite());// + "\nSwitch Count:" +switchCnt +"\nElite Updates"+updateElite );
+		//geneAl.showElite();
 	}
 	
 }
